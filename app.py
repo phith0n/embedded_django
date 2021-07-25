841304a9fb2c2ecc3c732b34cbf3c6c0cd204e33
phith0n <phith0n.ph2f@gmail.com>
2021-07-26 02:53:17 +0800

    add logging and middleware settings

--- app.py
+++ app.py
@@ -15,6 +15,24 @@ TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [os.path.join(BASE_DIR, 'templates')]
 }]
+MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
+]
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
+        },
+    },
+}
 
 
 def home(request):
